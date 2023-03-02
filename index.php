8e719b85813f04874945ec28ddcded478ad6cd7f
Oleg Fil <abyzswalker@gmail.com>
2023-03-03 01:58:12 +0200

    add

--- index.php
+++ index.php
@@ -3,6 +3,41 @@ $memcached = new Memcached();
 $memcached->addServer('jm-memcached1', 11211);
 $memcached->addServer('jm-memcached2', 11211);
 $memcached->addServer('jm-memcached3', 11211);
-$memcached->set('testKey', 'testValue');
-print_r($memcached->getResultMessage());
-print_r('<hr />');
+
+$num = $_GET['num'];
+$user = $_GET['user'] ?: 'admin';
+
+function fib($a)
+{
+    if ($a === 0) {
+        return 0;
+    }
+    if ($a === 1) {
+        return 1;
+    }
+    return fib($a - 1) + fib($a - 2);
+}
+
+// Задача 1
+//server/index.php?num=40 - выведет на экран страницу с 40м числом фибонначи
+//Рассчитанное число должно кешироваться в мэмкеш на 5 минут и не считаться повторно при запросе той же страницы 5 минут, после должно пересчитаться снова
+
+if ($memcached->get('fibKey')) {
+    echo $memcached->get('fibKey');
+} else {
+    $memcached->add('fibKey', fib($num), 300);
+    echo $memcached->get('fibKey');
+}
+
+// Задача 2
+//Предыдущий скрипт должен кешировать число фибонначи по пользователям.
+// Пользователь 1 запрашивает страницу server/index.php?num=50,
+// число рассчитывается и кешируются, Пользователь 1 при следующих запросах получает быстрый ответ.
+// При этом Пользователь 2 рассчитает число фибоначи для себя снова несмотря на остальных.
+
+if ($memcached->get($user)) {
+    echo $memcached->get($user);
+} else {
+    $memcached->add($user, fib($num), 300);
+    echo $memcached->get($user);
+}
